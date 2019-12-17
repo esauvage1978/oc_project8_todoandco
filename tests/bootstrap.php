4adf795ca8f7499a5550c7728a8f587dbf4b7999
SAUVAGE <emmanuel.sauvage@live>
2019-12-17 11:46:40 +0100

    #06 use sqllite for test

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,12 @@
 
 // tests/bootstrap.php
 if (isset($_ENV['BOOTSTRAP_FIXTURES_LOAD'])) {
+    $command = 'php '.dirname(__DIR__, 1).'/bin/console doctrine:database:drop --force';
+    passthru($command);
+    $command = 'php '.dirname(__DIR__, 1).'/bin/console doctrine:database:create';
+    passthru($command);
+    $command = 'php '.dirname(__DIR__, 1).'/bin/console doctrine:schema:create';
+    passthru($command);
     // executes the "php bin/console cache:clear" command
     $command = 'php '.dirname(__DIR__, 1).'/bin/console doctrine:fixtures:load -n --env=test';
     passthru($command);
