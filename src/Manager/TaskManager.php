27ab60793043b42fdc558f640643d2d3908393ae
SAUVAGE <emmanuel.sauvage@live>
2019-12-17 11:48:47 +0100

    #06 create manager

---
+++ src/Manager/TaskManager.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Manager;
+
+use App\Entity\Task;
+use App\Validator\TaskValidator;
+use Doctrine\ORM\EntityManagerInterface;
+
+class TaskManager
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    private $manager;
+
+    /**
+     * @var TaskValidator
+     */
+    private $validator;
+
+    public function __construct(
+        EntityManagerInterface $manager,
+        TaskValidator $validator
+    ) {
+        $this->manager = $manager;
+        $this->validator = $validator;
+    }
+
+    public function save(Task $task): bool
+    {
+        $this->initialise($task);
+
+        if (!$this->validator->isValid($task)) {
+            return false;
+        }
+
+        $this->manager->persist($task);
+        $this->manager->flush();
+
+        return true;
+    }
+
+    public function initialise(Task $task)
+    {
+        if (empty($task->getId())) {
+            $task->setCreatedAt(new \DateTime());
+            $task->setIsDone(false);
+        }
+    }
+
+    public function getErrors(Task $task)
+    {
+        return $this->validator->getErrors($task);
+    }
+}
